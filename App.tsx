2bb5dd7e701a2d45065d8e19f8951f0a5a42458b
UweBradl <105041819+UweBradl@users.noreply.github.com>
2023-03-01 12:43:01 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );    
+}
+
+      //       <Fragment>
+      //   <DatePicker
+      //   selected={startDate}
+      //   onChange={(date) => setStartDate(date)}
+      //   locale="en-GB"n
+      //   placeholderText="Weeks start on Monday"
+      //   />
+      // </Fragment>
