4188fbc41f7f700f7d4d931f2d8c6b81f9b8f595
UweBradl <105041819+UweBradl@users.noreply.github.com>
2023-03-01 12:55:40 +0100

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { DatePicker } from 'react-datepicker';
+
 import './style.css';
 
 export default function App() {
+
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
-    <div>
+    [<div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+    </div>, 
+    <div>
+        <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        locale="en-GB"n
+        placeholderText="Weeks start on Monday"
+        />
     </div>
+    ]
   );    
 }
 
-      //       <Fragment>
-      //   <DatePicker
-      //   selected={startDate}
-      //   onChange={(date) => setStartDate(date)}
-      //   locale="en-GB"n
-      //   placeholderText="Weeks start on Monday"
-      //   />
-      // </Fragment>
